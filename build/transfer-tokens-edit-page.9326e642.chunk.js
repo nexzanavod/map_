b63baf1d25ac4841aa831142a93b246ae0e52e13
nexzanavod <aanavod@gmail.com>
2023-10-12 14:34:59 +0530

    add

---
+++ build/transfer-tokens-edit-page.9326e642.chunk.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmy_project=self.webpackChunkmy_project||[]).push([[2492],{63252:(o,t,e)=>{e.r(t),e.d(t,{default:()=>P});var s=e(67294),E=e(31613),n=e(86706),_=e(36364),r=e(43467);const P=()=>{const a=(0,n.v9)(_._);return s.createElement(E.O4,{permissions:a.settings["transfer-tokens"].read},s.createElement(r.Z,null))}}}]);
